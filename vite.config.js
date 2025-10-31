4fb59ff58b5503c3842e140d8d8b3b7d2797dc2b
kollekka <adrianswieca491@gmail.com>
2025-10-31 14:56:17 +0100

    base change

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Ustaw base na nazwę repozytorium, jeśli hostujesz w GitHub Pages
-// Przykład: base: '/twoje-repo/'
-// Możesz też ustawić zmienną środowiskową VITE_BASE podczas builda
-const base = process.env.VITE_BASE || ''
+const base = '/Portfolio/'
 
 export default defineConfig({
   plugins: [react()],
